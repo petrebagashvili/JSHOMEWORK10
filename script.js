7355b22368bb8eb28f34600ad68839462be11ab2
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2024-12-11 23:22:39 +0400

    changed

--- script.js
+++ script.js
@@ -50,3 +50,24 @@ fetch("https://jsonplaceholder.typicode.com/posts",{
     .then(data => console.log(data))
     .catch(error => console.log(error))
 
+async function handleResponse(response) {
+    const { status } = response; 
+
+    if(status === 200){
+        console.log('200 OK: Successful request.');
+        return await response.json();
+    }
+    if (status === 201) {
+        console.log('201 Created: Successfully created resource.');
+        return await response.json();
+    }
+    if (status === 404) {
+        console.error('404 Not Found: If the post ID for deletion is not found.');
+        return await response.json();
+    }
+    if (status === 500) {
+        console.error('500 Internal Server Error: If there’s a server error.');
+        return await response.json();
+    }
+    
+}
